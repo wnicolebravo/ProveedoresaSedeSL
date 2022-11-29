3a8f50128a21f6c36f9ee6a7600a64622e7b02f7
Wanda Nicole Bravo <86751741+wnicolebravo@users.noreply.github.com>
2022-11-29 16:41:57 -0300

    Update SP_Productos_de_compra_minima.sql

--- SP_Productos_de_compra_minima.sql
+++ SP_Productos_de_compra_minima.sql
@@ -1,1 +1,7 @@
-DROP PROCEDURE Productos_de_menor500
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Productos_de_compra_minima`(IN p_IDProd int)
+BEGIN
+ SELECT IF ( pesoProd < 500 , "Peso menor a 500,comprar min 5 u", "Peso mayor a 500,comprar 1 u")as Resultado, nomprod nombre_producto
+ ,pesoProd peso_producto FROM Productos
+ WHERE IDProd = p_IDProd;
+
+ END
